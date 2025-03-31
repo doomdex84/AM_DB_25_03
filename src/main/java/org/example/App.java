9db0fa0a0239223aa37c0f8bcb20eeb5ed77c88f
doomdex84 <doomdex84@gmail.com>
2025-03-31 11:42:22 +0900

    수정, 삭제, 상세보기 구현, 글의 유무체크

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -54,7 +54,7 @@ public class App {
         }
     }
 
-    private int doAction(Connection conn, Scanner sc, String cmd) throws SQLException {
+    private int doAction(Connection conn, Scanner sc, String cmd) {
 
         if (cmd.equals("exit")) {
             return -1;
@@ -79,6 +79,7 @@ public class App {
 
             System.out.println(id + "번 글이 생성됨");
 
+
         } else if (cmd.equals("article list")) {
             System.out.println("==목록==");
 
@@ -88,7 +89,6 @@ public class App {
             sql.append("SELECT *");
             sql.append("FROM article");
             sql.append("ORDER BY id DESC");
-
 
             List<Map<String, Object>> articleListMap = DBUtil.selectRows(conn, sql);
 
@@ -116,33 +116,106 @@ public class App {
                 return 0;
             }
 
+            SecSql sql = new SecSql();
+            sql.append("SELECT *");
+            sql.append("FROM article");
+            sql.append("WHERE id = ?;", id);
+
+            Map<String, Object> articleMap = DBUtil.selectRow(conn, sql);
+
+            if (articleMap.isEmpty()) {
+                System.out.println(id + "번 글은 없음");
+                return 0;
+            }
+
             System.out.println("==수정==");
             System.out.print("새 제목 : ");
             String title = sc.nextLine().trim();
             System.out.print("새 내용 : ");
             String body = sc.nextLine().trim();
 
-            SecSql sql = new SecSql();
-
+            sql = new SecSql();
             sql.append("UPDATE article");
-            sql.append("SET updateDate = NOW(),");
-
+            sql.append("SET updateDate = NOW()");
             if (title.length() > 0) {
-                sql.append("title = ?,", title);//
-                }
+                sql.append(", title = ?", title);
+            }
             if (body.length() > 0) {
-                sql.append("`body` = ?;", body);
-                }
-            sql.append("WHERE id = ?",id);
+                sql.append(",`body` = ?", body);
+            }
+            sql.append("WHERE id = ?;", id);
 
             DBUtil.update(conn, sql);
 
-
-
-
-
             System.out.println(id + "번 글이 수정되었습니다.");
+        } else if (cmd.startsWith("article detail")) {
+
+            int id = 0;
+
+            try {
+                id = Integer.parseInt(cmd.split(" ")[2]);
+            } catch (Exception e) {
+                System.out.println("번호는 정수로 입력해");
+                return 0;
+            }
+
+
+
+
+
+            SecSql sql = new SecSql();
+            sql.append("SELECT *");
+            sql.append("FROM article");
+            sql.append("WHERE id = ?;", id);
+
+            Map<String, Object> articleMap = DBUtil.selectRow(conn, sql);
+
+            if (articleMap.isEmpty()) {
+                System.out.println(id + "번 글은 없음");
+                return 0;
+            }
+
+            Article article = new Article(articleMap);
+
+            System.out.println("번호 : " + article.getId());
+            System.out.println("작성날짜 : " + article.getRegDate());
+            System.out.println("수정날짜 : " + article.getUpdateDate());
+            System.out.println("제목 : " + article.getTitle());
+            System.out.println("내용 : " + article.getBody());
+        } else if (cmd.startsWith("article delete")) {
+
+            int id = 0;
+
+            try {
+                id = Integer.parseInt(cmd.split(" ")[2]);
+            } catch (Exception e) {
+                System.out.println("번호는 정수로 입력해");
+                return 0;
+            }
+
+            SecSql sql = new SecSql();
+            sql.append("SELECT *");
+            sql.append("FROM article");
+            sql.append("WHERE id = ?;", id);
+
+            Map<String, Object> articleMap = DBUtil.selectRow(conn, sql);
+
+            if (articleMap.isEmpty()) {
+                System.out.println(id + "번 글은 없음");
+                return 0;
+            }
+
+            System.out.println("==삭제==");
+            sql = new SecSql();
+            sql.append("DELETE FROM article");
+            sql.append("WHERE id = ?;", id);
+
+            DBUtil.delete(conn, sql);
+
+            System.out.println(id + "번 글이 삭제되었습니다.");
         }
+
+
         return 0;
     }
 }
